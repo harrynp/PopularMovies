cdd76a2cd352a6a8296248ba2d4607d9e065f5d5
Harry <harrynp12@gmail.com>
2017-09-27 17:37:07 -0700

    Added template settings and check for internet; removed MovieDB api key

---
+++ app/src/main/java/com/github/harrynp/popularmovies/MainActivity.java
@@ -0,0 +1,23 @@
+package com.github.harrynp.popularmovies;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+}
